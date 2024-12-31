eef644c3aaa55dea62961847fce6e93636838dd5
jim@vheavy.com <jim@vheavy.com>
2024-12-30 23:32:35 -0800

    Working direct read

--- Makefile
+++ Makefile
@@ -11,5 +11,24 @@ GIT_VERSION := "$(shell git describe --abbrev=4 --dirty --always --tags)"
 BUILD_EXTRA_FLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
 
 
+csim:       	
+	g++ -x c++ -g espLogicAnalyzer.ino -o $@ -DESP32 -DCSIM -DUBUNTU -I./ -I${HOME}/Arduino/lib -I ${HOME}/Arduino/libraries/esp32jimlib/src/ 
+
+
 include ${HOME}/Arduino/libraries/makeEspArduino/makeEspArduino.mk
+
+fixtty:
+	stty -F ${UPLOAD_PORT} -hupcl -crtscts -echo raw 115200 
+
+cat:    fixtty
+	cat ${UPLOAD_PORT}
+
+uc:
+	make upload && make cat
+
+
+backtrace:
+	tr ' ' '\n' | /home/jim/.arduino15/packages/esp32/tools/xtensa-esp32-elf-gcc/*/bin/xtensa-esp32-elf-addr2line -f -i -e /tmp/mkESP/winglevlr_ttgo-t1/*.elf
+        
+
 
--- espLogicAnalyzer.ino
+++ espLogicAnalyzer.ino
@@ -1,20 +1,43 @@
+#ifdef UBUNTU
+#include "ESP32sim_ubuntu.h"
+#else // #ifndef UBUNTU
 #include <Adafruit_NeoPixel.h> 
-Adafruit_NeoPixel pixels(1, 21, NEO_GRB + NEO_KHZ800);
+#endif
+
+#include "RollingLeastSquares.h"
+//Adafruit_NeoPixel pixels(1, 21, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  pixels.begin();
 }
 
-unsigned char red = 0;
+RollingLeastSquaresStatic<int, float, 4> avg;
+
+uint32_t lastMicro = 0;
+int reads = 0;
+
+
+float lastStableValue = 0;
+float rmsStableThreshold = 3;
+float minDifference = 100;
+uint32_t lastStableTime = 0;
+
 void loop() {
-  const int maxLum = 50;
-  pixels.clear();
-  if (digitalRead(0)) { 
-	pixels.setPixelColor(0, pixels.Color(random(maxLum), random(maxLum), random(maxLum)));
-  } else { 
-	pixels.setPixelColor(0, 0,0,red+=20);
+  int x = analogRead(1);
+  avg.add(0, x);
+  float rms = avg.rmsError();
+  float currentAvg = avg.averageY();
+  reads++;
+  uint32_t now = micros();
+  if (lastMicro / 1000000 != now / 1000000) {  
+    printf("poop %08.2f %08.2f %04d %d \n", currentAvg, rms, x, reads);
+    reads = 0;
   }
-  pixels.show();
-  delay(50);
-  printf("poop %d\n", (int)red);
+
+  if (rms < rmsStableThreshold && abs(lastStableValue - currentAvg) > minDifference) { 
+    printf("%08.2f %08.2f %d\n", currentAvg, rms, (int)(now - lastStableTime));
+    lastStableTime = now;
+    lastStableValue = currentAvg;
+
+  }
+  lastMicro = now;
 }
